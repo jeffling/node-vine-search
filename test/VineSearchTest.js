c7dcdcbec86c97148acf53a540639578e943a502
Jeffrey Ling <jeff.ling@utoronto.ca>
2013-10-20 18:42:26 -0700

    cleanup, using promise chain in search

--- test/VineSearchTest.js
+++ test/VineSearchTest.js
@@ -4,16 +4,36 @@ var
   chai = require('chai'),
   should = chai.should(),
   chaiAsPromised = require("chai-as-promised"),
-  vine = require("../lib/VineSearch.js");
+  vine = require("../lib/VineSearch.js"),
+  when = require("when");
 
 require("mocha-as-promised")();
 chai.use(chaiAsPromised);
 
+var tweetvinetemplate = {
+  user: 'String',
+  id : 'String',
+  text : 'String',
+  vineUrl : 'String',
+  vidUrl : 'String',
+  thumbUrl : 'String'
+}; // example tweetvine object for comparisons
+
 describe("vine", function() {
   describe("search", function() {
-    it("should return a list of tweet/vine (tvine?) objects ", function() {
-      var result = vine.search();
-      return result.should.eventually.have.length(10);
+    var testSearch = vine.search();
+
+    it("should return a list of tweet/vine (tweetvine?) objects ", function() {
+      return testSearch.should.eventually.have.length(10);
+    });
+
+    it("each object in the list should have certain properties", function() {
+      var keys = [];
+      for(var k in tweetvinetemplate) keys.push(k);
+
+      return when.map(testSearch, function(tweetvine) {
+        return tweetvine.should.have.keys(keys);
+      })
     });
   });
 });
